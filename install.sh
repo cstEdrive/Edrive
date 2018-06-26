bcc547787a5ad4fd9c4f0152430e17a689f2f488
Martin Plantinga <martinplantinga@dyn-065009.nbw.tue.nl>
2018-06-26 10:50:51 +0200

    install file update

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# INSTALL SCRIPT FOR EDRIVE
+
+sudo apt-get update -qq
+sudo apt-get install -yy htop gdebi synaptic grsync bleachbit gufw
+
+sudo apt-get install -yy 	libpq-dev \
+							net-tools \
+							software-properties-common \
+							git \
+							rpcbind \
+							nfs-common \
+							network-manager-pptp \
+							libgtk2.0-dev \
+							oracle-java6-installer \
+							gedit \
+							ssh \
+							make \
+							libpq-dev \
+							ethtool \
+							postgresql \
+							postgresql-client -y
+
+
+
+
+chown -R root:root /home/ebox/svn
+
+chmod -R 755 /home/ebox/svn
+
+
+
+
+banner "INSTALLATION COMPLETE"
+
+
+
+
+echo "Modifications to be made manually"
+
+echo "1. Within /home/ebox/svn/trunk/src/E-box/Targets/ectarget/ec_main.c"
+
+echo "   change line 560 to:"
+
+echo "   /usr/local/MATLAB/R2015b/rtw/c/ectarget/ectarget.u1"
+
+
+
