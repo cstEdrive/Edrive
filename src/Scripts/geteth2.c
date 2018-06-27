f6ff414b8d811e82024f55ac4e617e4a8928ccd7
Martin Plantinga <martinplantinga@dyn-065009.nbw.tue.nl>
2018-06-27 13:02:30 +0200

    upd

--- src/Scripts/geteth2.c
+++ src/Scripts/geteth2.c
@@ -21,34 +21,33 @@ int main(int argc, char **argv)
             ec_stop();
             printf("\n\nEhtercat stack connected to %s ...\n", argv[i]);
             /*printf("\n\nEtherCAT stack connected to eth2, run changeeth(2) in matlab\n");*/
-            return 2;
+            return 0;
         }
-        // printf("%s\n",argv[i]);
     }
     
-    if ( (iret=ec_start("eth2"))==EC_SUCCESS ) {
-        ec_stop();
-        printf("\n\nEhtercat stack connected to eth2 ...\n");
-        /*printf("\n\nEtherCAT stack connected to eth2, run changeeth(2) in matlab\n");*/
-        return 2;
-    }
-
-    if ( (iret=ec_start("eth1"))==EC_SUCCESS ) {
-        ec_stop();
-        printf("\n\nEhtercat stack connected to eth1 ...\n");
-        /*printf("\n\nEtherCAT stack connected to eth1, run changeeth(1) in matlab\n");*/
-        return 1;
-    }
-    
-    if ( (iret=ec_start("eth0"))==EC_SUCCESS ) {
-        ec_stop();
-        printf("\n\nEhtercat stack connected to eth0 ...\n");
-        /*printf("\n\nEtherCAT stack connected to eth0, run changeeth(0) in matlab\n");*/
-    return 0;
-    }
-
-
     printf("\n\nNo EtherCAT stack found, connect EtherCAT to pc and run as root\n");
     return -1;
+    
+    // if ( (iret=ec_start("eth2"))==EC_SUCCESS ) {
+    //     ec_stop();
+    //     printf("\n\nEhtercat stack connected to eth2 ...\n");
+    //     /*printf("\n\nEtherCAT stack connected to eth2, run changeeth(2) in matlab\n");*/
+    //     return 2;
+    // }
+
+    // if ( (iret=ec_start("eth1"))==EC_SUCCESS ) {
+    //     ec_stop();
+    //     printf("\n\nEhtercat stack connected to eth1 ...\n");
+    //     /*printf("\n\nEtherCAT stack connected to eth1, run changeeth(1) in matlab\n");*/
+    //     return 1;
+    // }
+    
+    // if ( (iret=ec_start("eth0"))==EC_SUCCESS ) {
+    //     ec_stop();
+    //     printf("\n\nEhtercat stack connected to eth0 ...\n");
+    //     /*printf("\n\nEtherCAT stack connected to eth0, run changeeth(0) in matlab\n");*/
+    // return 0;
+    }
+
         
 }
