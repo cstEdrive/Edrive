963e7ddd77c43213d20476671bf7e86d8dc7e257
Martin Plantinga <martinplantinga@dyn-065009.nbw.tue.nl>
2018-06-27 11:14:01 +0200

    upd

--- src/Scripts/geteth.c
+++ src/Scripts/geteth.c
@@ -15,7 +15,8 @@ int main(void)
 {
         int iret;
 
-        char s[]=system(SHELLSCRIPT);
+        char s[] = system(SHELLSCRIPT);
+        int status = system("gzip foo");
         printf(s);
         
         if ( (iret=ec_start("eth2"))==EC_SUCCESS ) {
