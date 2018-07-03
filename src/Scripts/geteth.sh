7b03bb079e53cc9f1026fe96ea1a595fd02fc96c
Martin Plantinga <martinplantinga@Martins-MacBook-Pro.local>
2018-07-03 19:00:38 +0200

    modified return value of geteth

--- src/Scripts/geteth.sh
+++ src/Scripts/geteth.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/usr/bin/geteth.o `ifconfig -a | sed 's/[ \t].*//;/^$/d' | sed s/://g` | grep Ethercat
+OUTPUT="$(/usr/bin/geteth.o `ifconfig -a | sed 's/[ \t].*//;/^$/d' | sed s/://g` | grep Ethercat)"
 
 
