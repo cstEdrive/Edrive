3d3b6a57c329862860f3264b1462f91be595dc5e
Martin Plantinga <martinplantinga@Martins-MacBook-Pro.local>
2018-07-03 20:58:24 +0200

    modified return value of geteth

--- src/Apps/E-Scope/guiv25/qs.m
+++ src/Apps/E-Scope/guiv25/qs.m
@@ -89,7 +89,7 @@ netif = ['eth',num2str(netif)];
 eth_port = regexp(geteth_output, 'Ethercat stack connected to ', 'split');
 eth_port = eth_port{2}(1:end-1)
 
-geteth_path = sprintf('/home/ebox/Edrive/src/Apps/E-Scope/engine/eseng %d %d %d %s &',maxfreq, frame_length, dac_ndown, eth_port);
+geteth_path = sprintf('/home/ebox/Edrive/src/Apps/E-Scope/engine/eseng %d %d %d %s &',maxfreq, frame_length, dac_ndown, eth_port)
 system(geteth_path);
 
 % wait some time
