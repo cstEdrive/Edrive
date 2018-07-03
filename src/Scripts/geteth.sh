3d597da4f2cc92a1a76ddf81fff31bde94b75054
Martin Plantinga <martinplantinga@Martins-MacBook-Pro.local>
2018-07-03 19:27:57 +0200

    modified return value of geteth

--- src/Scripts/geteth.sh
+++ src/Scripts/geteth.sh
@@ -4,8 +4,8 @@ OUTPUT="$(/usr/bin/geteth.o `ifconfig -a | sed 's/[ \t].*//;/^$/d' | sed s/://g`
 
 echo "${OUTPUT}"
 
-ETHERCAT=`echo '${OUTPUT}' | grep -oP "Ethercat stack connected to \K(.+)"
+echo "${OUTPUT}" | grep -oP "Ethercat stack connected to \K(.+)"
  # grep Ethercat $(OUTPUT)
-echo "${ETHERCAT}"
+# echo "${ETHERCAT}"
 
 
