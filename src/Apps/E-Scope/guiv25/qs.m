15c795fa299f985232a30413b2b142a00116dba8
Martin Plantinga <m.plantinga@tue.nl>
2018-07-04 10:49:39 +0200

    removed comments and others

--- src/Apps/E-Scope/guiv25/qs.m
+++ src/Apps/E-Scope/guiv25/qs.m
@@ -35,45 +35,12 @@ clear mex;
 maxfreq=4096;
 frame_length = maxfreq;
 dac_ndown=0;
-netif=0;
 
-% Override defaults by mat file created by changeeth for changing default
-% netif
-if (exist('eth_defaults.mat','file')==2)
-    load eth_defaults.mat
-end
-
-if nargin>0
-    maxfreq=varargin{1,1};
-end
-if nargin>1
-    frame_length=varargin{1,2};
-end
-if nargin>2
-    dac_ndown=varargin{1,3};
-end
-if nargin>3
-    netif=varargin{1,4};
-end
-
-netif = ['eth',num2str(netif)];
-
-%cmd=sprintf('!rxvt -title "QadScope engine" -e preempt_run_usb /usr/local/matlabr14sp2/toolbox/qadscope/qseng_usb_preempt %f %d %d &',maxfreq,dac_ndown,ical);
-% cmd=sprintf('!"E-Scope engine" -e "taskset 02 /home/ebox/svn/trunk/src/E-box/Apps/E-Scope/engine/eseng %d %d %d %s " &',maxfreq,frame_length,dac_ndown,netif);
-% cmd=sprintf('!gnome-terminal -t "E-Scope engine" -e "taskset 02 /home/ebox/svn/trunk/src/E-box/Apps/E-Scope/engine/eseng" &');
-% eseng_path=sprintf('!gnome-terminal -t "/home/ebox/svn/trunk/src/E-box/Apps/E-Scope/engine/eseng"');
-% eval(cmd)
-
-% eval(cmd);
-
-% system('/home/ebox/svn/trunk/src/E-box/Apps/E-Scope/engine/eseng &');
-
-% eth_port = system('geteth');
 [status, geteth_output] = system('geteth');
-eth_port = regexp(geteth_output, 'Ethercat stack connected to ', 'split');
-eth_port = eth_port{2}(1:end-1);
+netw_interface_name = regexp(geteth_output, 'Ethercat stack connected to ', 'split');
+netw_interface_name = netw_interface_name{2}(1:end-1);
 
-geteth_path = sprintf('/home/ebox/Edrive/src/Apps/E-Scope/engine/eseng %d %d %d %s &',maxfreq, frame_length, dac_ndown, eth_port);
+geteth_path = sprintf('/home/ebox/Edrive/src/Apps/E-Scope/engine/eseng %d %d %d %s &',maxfreq, frame_length, dac_ndown, netw_interface_name);
 system(geteth_path);
 
 % wait some time
